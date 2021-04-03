a828cff5b86e032b1889d86caf8f29f58c383cac
Rafael Anacleto <rafael.anacleto12@gmail.com>
2021-04-03 13:27:35 -0300

    feat: games store, update and delete

--- api.n64.net/src/routes/games.js
+++ api.n64.net/src/routes/games.js
@@ -7,7 +7,6 @@ const DEFAULT_LIMIT = 10;
 
 router.get("/", async (req, res) => {
   const { limit, page, fields, orderBy, sortBy, q } = req.query;
-
   const criteria = {
     limit: Number(limit) || DEFAULT_LIMIT,
     page: Number(page) || DEFAULT_PAGE,
@@ -16,10 +15,32 @@ router.get("/", async (req, res) => {
     sortBy: sortBy !== undefined ? Number(sortBy) : 1,
     q: q || "",
   };
-
   const result = await Games.find(criteria);
 
-  return res.json({ message: "Games OK", data: result });
+  return res.json({ message: "Games list", data: result });
+});
+
+router.post("/", async (req, res) => {
+  const { body } = req;
+  const data = await Games.store(body);
+
+  return res.json({ message: "Stored game", data: data });
+});
+
+router.put("/:id", async (req, res) => {
+  const { body, params } = req;
+  const { id } = params;
+  const game = await Games.update(id, body);
+
+  return res.json({ message: "Game updated", data: { game } });
+});
+
+router.delete("/:id", async (req, res) => {
+  const { params } = req;
+  const { id } = params;
+  const result = await Games.destroy(id);
+
+  return res.json({ message: "Game deleted", data: result });
 });
 
 module.exports = router;
